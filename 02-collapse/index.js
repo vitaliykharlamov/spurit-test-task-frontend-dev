219de88f52b6fed34dd71a49171403cbec325a8d
«vitaliykharlamov» <vitaliykharlamov@gmail.com>
2022-08-28 16:16:14 +0300

    completed second task

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,40 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const button = document.querySelector('.collapsible__button');
+const textBtnVisible = document.querySelector('.collapsible__action--visible');
+const textBtnHidden = document.querySelector('.collapsible__action--hidden');
+const content = document.querySelector('.collapsible__content');
+
+let isShowContent;
+
+function showContent() {
+    textBtnVisible.style.display = 'block';
+    textBtnHidden.style.display = 'none';
+    isShowContent = true;
+}
+
+function hideContent() {
+    textBtnVisible.style.display = 'none';
+    textBtnHidden.style.display = 'block';
+    isShowContent = false;
+}
+
+hideContent();
+
+const animateContent = content.animate([
+    {transform: 'translateX(-100%)'}, 
+    {transform: 'translateX(0%)'}
+], {
+    duration: 500,
+    fill: 'backwards'
+});
+
+animateContent.pause();
+
+button.addEventListener('click', () => {
+    if (isShowContent) {
+        hideContent();
+        animateContent.reverse();
+    } else {
+        showContent();
+        animateContent.play();
+    }
+});
